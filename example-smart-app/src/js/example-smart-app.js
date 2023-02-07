bca06742d708882be53c80a123c3c0ee877362e4
CanadianBaconHD <chllafrance@gmail.com>
2023-02-07 16:50:43 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -65,8 +65,9 @@
 					for(var j=0,jLen=allergies[i].reaction.length;j<jLen;j++) {
 						reactionStr.push(allergies[i].reaction[j].manifestation[0].text);
 					}
-				}
 				allergyTable += "<tr><td>"+allergies[i].code.text+"</td></td>"+reactionStr.join(", ")+"</td></tr>";
+				}
+				
 		  }
 		  
 		
